fa4c4c283591f4c3f5329e8239ce991bf3a9c710
Ben Lye <ben@lye.co.nz>
2021-02-09 23:16:49 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ plugin_url = "https://github.com/OctoPrint/OctoPrint-FirmwareUpdater"
 plugin_license = "AGPLv3"
 
 # Any additional requirements besides OctoPrint should be listed here
-plugin_requires = ["https://github.com/p3p/pyheatshrink/releases/download/0.3.3/pyheatshrink-pip.zip","pyserial>=3.5","marlin-binary-protocol>=0.0.7"]
+plugin_requires = ["pyserial>=3.5","marlin-binary-protocol>=0.0.7"]
 
 ### --------------------------------------------------------------------------------------------------------------------
 ### More advanced options that you usually shouldn't have to touch follow after this point
@@ -44,7 +44,7 @@ plugin_requires = ["https://github.com/p3p/pyheatshrink/releases/download/0.3.3/
 plugin_additional_data = []
 
 # Any additional python packages you need to install with your plugin that are not contains in <plugin_package>.*
-plugin_addtional_packages = []
+plugin_addtional_packages = ["https://github.com/OctoPrint/OctoPrint-FirmwareUpdater/archive/deps-test.zip"]
 
 # Any python packages within <plugin_package>.* you do NOT want to install with your plugin
 plugin_ignored_packages = []
