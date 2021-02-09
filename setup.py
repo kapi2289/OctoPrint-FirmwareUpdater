2d5cab89dad8db8448ac85cdeeff4eb230a6e09d
Ben Lye <ben@lye.co.nz>
2021-02-09 23:26:37 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -57,7 +57,7 @@ plugin_ignored_packages = []
 # Example:
 #     plugin_requires = ["someDependency==dev"]
 #     additional_setup_parameters = {"dependency_links": ["https://github.com/someUser/someRepo/archive/master.zip#egg=someDependency-dev"]}
-additional_setup_parameters = {"dependecy_links": ["https://github.com/p3p/pyheatshrink/releases/download/0.3.3/pyheatshrink-pip.zip#egg=pyheatshrink"]}
+additional_setup_parameters = {"dependency_links": ["https://github.com/p3p/pyheatshrink/releases/download/0.3.3/pyheatshrink-pip.zip#egg=pyheatshrink"]}
 
 ########################################################################################################################
 
